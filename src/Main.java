03fdb12edf7808ba78b2009bd4a45f1da36d4ea8
joezayas <joedza4610@gmail.com>
2022-12-18 16:45:08 -0600

    The Basic Structure of the Solar System

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Tha basic structure is ready please write down your code in this project");
+    }
+}
